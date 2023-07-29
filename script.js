4c19aa1e01af0267ec09576d0d42fc801097496a
OZnrl <108094114+OZnrl@users.noreply.github.com>
2023-07-29 23:41:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("test");
+
+function activeMB(x){
+x.classList.toggle("activeMB");
+}
+
+
+
+
+
+
+function openNav(z){
+    document.getElementsByName("nav").style.width="20px"
+    
+}
+
+
+
+              
+
